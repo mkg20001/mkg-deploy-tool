1f02a022f5a8fac2453948e2c9fd046eb82be816
Maciej Krüger <mkg20001@gmail.com>
2018-08-18 14:06:51 +0200

    fix: Multi-var escape

--- src/escapeWithVar.js
+++ src/escapeWithVar.js
@@ -7,6 +7,6 @@ module.exports = (args) => {
   return args.map(arg => {
     let escaped = shellEscape([arg])
     let quote = escaped[0]
-    return escaped.replace(/\$(\{[^}]+\}|[a-z0-9A-Z_]+)/, (r) => quote + dblQuote + r + dblQuote + quote)
+    return escaped.replace(/\$(\{[^}]+\}|[a-z0-9_]+)/gmi, (r) => quote + dblQuote + r + dblQuote + quote)
   }).join(' ')
 }
