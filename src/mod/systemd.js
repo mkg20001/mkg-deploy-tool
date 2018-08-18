509fe065941bf12f082856679ec6260956df7242
Maciej Krüger <mkg20001@gmail.com>
2018-08-18 15:49:22 +0200

    feat: Add systemd module

---
+++ src/mod/systemd.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const utils = require('../utils')
+
+module.exports = (config) => { // config is an array of entries about which rules to add/rm
+  return Object.keys(config).map(service => {
+    const serviceFile = config[service]
+
+    return utils.wrap('systemd', service, {
+      install: utils.tree()
+        .cmd('ln', '-sf', '$MAINFOLDER/' + serviceFile, '/etc/systemd/system/' + service + '.service')
+        .cmd('systemctl', 'daemon-reload')
+        .cmd('systemctl', 'enable', service)
+        .cmd('systemctl', 'start', service)
+        .str(),
+      remove: utils.tree()
+        .cmd('systemctl', 'stop', service)
+        .cmd('systemctl', 'disable', service)
+        .cmd('rm', '-f', '/etc/systemd/system/' + service + '.service')
+        .cmd('systemctl', 'daemon-reload')
+      // TODO: daemon-reload and restart service if service file hash changed
+    })
+  })
+}
