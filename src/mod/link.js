654bb0ece3eb890873033b0c5e49750a09238258
Maciej Krüger <mkg20001@gmail.com>
2018-08-22 10:14:06 +0200

    fix: Upgrade links

--- src/mod/link.js
+++ src/mod/link.js
@@ -5,7 +5,7 @@ const utils = require('../utils')
 module.exports = (config, file, main) => { // config is an array of entries about which rules to add/rm
   return Object.keys(config).map((from, i) => {
     let to = config[from]
-    return utils.wrap('link', from, {
+    return utils.wrap('link', from + '*' + to, {
       install: utils.tree()
         .if(`[ -e "${to}" ]`, utils.tree().cmd('mv', '-v', `${to}`, `${to}.bak`))
         .cmd('ln', '-sf', `${main.mainFolder}/${from}`, to)
