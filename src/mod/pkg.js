9714a7f7e31ba8c40722e43e9560c8673dc31f3a
Maciej Krüger <mkg20001@gmail.com>
2018-08-22 11:01:06 +0200

    feat: PKG remove

--- src/mod/pkg.js
+++ src/mod/pkg.js
@@ -6,6 +6,7 @@ module.exports = (config) => { // config is an array of entries about which rule
   return config.map((pkg) =>
     utils.wrap('pkg', pkg, {
       install: utils.shellEscape(['apt-get', 'install', '-y', pkg]),
+      remove: utils.shellEscape(['apt-get', 'remove', '-y', pkg]),
       displayName: 'apt package ' + pkg,
       priority: 15
     })
