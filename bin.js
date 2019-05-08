bbff02ea79fe18f8804ea175f48595675ad74439
Maciej Krüger <mkg20001@gmail.com>
2019-05-08 14:27:10 +0200

    fix: agent not working

--- bin.js
+++ bin.js
@@ -43,7 +43,7 @@ if (process.argv[3] === 'oneline') {
         cmd: `echo "${b64}" | base64 -d | ${algo} -dc | sudo bash -`
       }
     })
-    .concat([{algo: 'plain', cmd: `echo "${Buffer.from(out).toString('base64')}" | base64 -d | sudo bash -`}])
+    .concat([{algo: 'plain', cmd: `echo "${Buffer.from(out).toString('base64')}" | base64 -d | sudo bash -E -`}])
     .sort((a, b) => a.length - b.length)[0]
   console.error('Compressed with %s', shortest.algo)
   console.log(shortest.cmd)
